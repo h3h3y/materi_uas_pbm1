f80a881c6bd33d13a66c519d8ab0fdfbc1015762
h3h3y <is4561722@gmail.com>
2024-01-24 15:35:05 +0700

    update

--- input_smartphone.dart
+++ input_smartphone.dart
@@ -9,6 +9,7 @@ class InputSmartphone extends StatefulWidget {
 }
 
 class _InputSmartphoneState extends State<InputSmartphone> {
+  // Dropdown Button
   String selectItem = 'Pilih Produsen';
   var valueItem = [
     'Pilih Produsen',
@@ -18,12 +19,21 @@ class _InputSmartphoneState extends State<InputSmartphone> {
     'Vivo',
     'Xiaomi'
   ];
+
+  // Radio Button
   int selectValue = 1;
+
+  // Checkbox
   bool isCheck1 = false;
   bool isCheck2 = false;
   bool isCheck3 = false;
   bool isCheck4 = false;
   bool isCheck5 = false;
+
+  // Switch Button
+  bool isSwitch = false;
+
+  // Form Input Field
   TextEditingController tecNama = TextEditingController();
   TextEditingController tecKode = TextEditingController();
   TextEditingController tecSeri = TextEditingController();
@@ -109,6 +119,90 @@ class _InputSmartphoneState extends State<InputSmartphone> {
                   selectItem = value!;
                 });
               },
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.only(left: 10),
+            child: Column(
+              children: [
+                const ListTile(
+                  title: Text('Contoh CheckBox'),
+                ),
+                ListTile(
+                  title: const Text('Box 1'),
+                  leading: Checkbox(
+                    value: isCheck1,
+                    onChanged: (value) {
+                      setState(() {
+                        isCheck1 = value!;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Box 2'),
+                  leading: Checkbox(
+                    value: isCheck2,
+                    onChanged: (value) {
+                      setState(() {
+                        isCheck2 = value!;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Box 3'),
+                  leading: Checkbox(
+                    value: isCheck3,
+                    onChanged: (value) {
+                      setState(() {
+                        isCheck3 = value!;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Box 4'),
+                  leading: Checkbox(
+                    value: isCheck4,
+                    onChanged: (value) {
+                      setState(() {
+                        isCheck4 = value!;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Box 5'),
+                  leading: Checkbox(
+                    value: isCheck5,
+                    onChanged: (value) {
+                      setState(() {
+                        isCheck5 = value!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.only(left: 10),
+            child: Column(
+              children: [
+                ListTile(
+                  title: const Text('Contoh Switch Button'),
+                  leading: Switch(
+                      value: isSwitch,
+                      onChanged: (value) {
+                        setState(() {
+                          isSwitch = value;
+                        });
+                      }),
+                ),
+              ],
             ),
           ),
           Container(
